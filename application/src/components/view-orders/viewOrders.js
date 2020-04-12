8590a3c8e2835e45382dbb55905ec48419e09ff9
Manish <manish@Manishs-MacBook-Pro.local>
2020-04-12 02:37:16 -0400

    feat(viewOrders): added frontend functionality for the edit and delete button

--- application/src/components/view-orders/viewOrders.js
+++ application/src/components/view-orders/viewOrders.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Template } from '../../components';
 import { SERVER_IP } from '../../private';
+import { Link } from 'react-router-dom';
 import './viewOrders.css';
 
 class ViewOrders extends Component {
@@ -12,12 +13,29 @@ class ViewOrders extends Component {
         fetch(`${SERVER_IP}/api/current-orders`)
             .then(response => response.json())
             .then(response => {
-                if(response.success) {
+                if (response.success) {
                     this.setState({ orders: response.orders });
                 } else {
                     console.log('Error getting orders');
                 }
             });
+    }
+
+    deleteOrder(orderId) {
+        fetch(`${SERVER_IP}/api/delete-order`, {
+            method: 'POST',
+            body: JSON.stringify({
+                id: orderId,
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(() => {
+                const orders = this.state.orders.filter(order => order.id !== orderId);
+                this.setState({ orders })
+            })
+            .catch(error => console.error(error));
     }
 
     render() {
@@ -35,11 +53,11 @@ class ViewOrders extends Component {
                                 <div className="col-md-4 d-flex view-order-middle-col">
                                     <p>Order placed at {`${createdDate.getHours()}:${createdDate.getMinutes()}:${createdDate.getSeconds()}`}</p>
                                     <p>Quantity: {order.quantity}</p>
-                                 </div>
-                                 <div className="col-md-4 view-order-right-col">
-                                     <button className="btn btn-success">Edit</button>
-                                     <button className="btn btn-danger">Delete</button>
-                                 </div>
+                                </div>
+                                <div className="col-md-4 view-order-right-col">
+                                    <Link to={`/order/${order._id}`} className="btn btn-success">Edit</Link>
+                                    <button className="btn btn-danger" onClick={() => this.deleteOrder(order._id)}>Delete</button>
+                                </div>
                             </div>
                         );
                     })}
