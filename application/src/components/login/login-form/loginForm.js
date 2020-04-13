97b4822f9b209d51306a326c8b4efffcaaef22c3
Manish <manish@Manishs-MacBook-Pro.local>
2020-04-12 20:00:11 -0400

    refactor(loginForm/authActions): loginUser action and login method modified for async calls

--- application/src/components/login/login-form/loginForm.js
+++ application/src/components/login/login-form/loginForm.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'; 
+import { connect } from 'react-redux';
 import { loginUser } from '../../../redux/actions/authActions'
 
 const mapActionsToProps = dispatch => ({
   commenceLogin(email, password) {
-    dispatch(loginUser(email, password))
+    return dispatch(loginUser(email, password))
   }
 })
 
@@ -14,9 +14,9 @@ class LoginForm extends Component {
     password: "",
   }
 
-  login(e) {
+  async login(e) {
     e.preventDefault();
-    this.props.commenceLogin(this.state.email, this.state.password);
+    await this.props.commenceLogin(this.state.email, this.state.password);
     this.props.onLogin();
   }
 
@@ -29,14 +29,14 @@ class LoginForm extends Component {
       <form>
         <div className="form-group">
           <label htmlFor="inputEmail">Email</label>
-          <input type="text" className="form-control"  placeholder="test@test.com" value={this.state.email} onChange={e => this.onChange('email', e.target.value)}></input>
+          <input type="text" className="form-control" placeholder="test@test.com" value={this.state.email} onChange={e => this.onChange('email', e.target.value)}></input>
         </div>
         <div className="form-group">
           <label htmlFor="inputPassword">Password</label>
           <input type="password" className="form-control" id="inputPassword" value={this.state.password} onChange={e => this.onChange('password', e.target.value)}></input>
         </div>
         <div className="d-flex justify-content-center">
-            <button onClick={e => this.login(e)} type="submit" className="btn btn-primary">Login</button>
+          <button onClick={e => this.login(e)} type="submit" className="btn btn-primary">Login</button>
         </div>
       </form>
     );
